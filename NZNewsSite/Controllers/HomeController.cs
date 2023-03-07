8a7e569d3a4b241e54da4805c814fa3e86335989
Sean Crump <116034584+Ess-See76@users.noreply.github.com>
2023-03-07 16:13:40 +1300

    Sports section + images added
    Sports section added to site, along with images

--- NZNewsSite/Controllers/HomeController.cs
+++ NZNewsSite/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace NZNewsSite.Controllers
             return View();
         }
 
+        public IActionResult Sports()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
